638be54e6b3318733f22474c24697f3433eabab6
Pypoh <nafifb@programmer.net>
2018-03-28 14:26:26 +0700

    initial commit

--- app/src/main/java/pypoh/project/com/palapa/LoginPage.java
+++ app/src/main/java/pypoh/project/com/palapa/LoginPage.java
@@ -1,13 +1,111 @@
 package pypoh.project.com.palapa;
 
+import android.content.Intent;
+import android.media.Image;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.style.TabStopSpan;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginPage extends AppCompatActivity {
+
+    private EditText mEditTextEmail;
+    private EditText mEditTextPassword;
+    FirebaseUser user;
+    FirebaseAuth mAuth;
+    private ImageButton mBtnLogin;
+    private ImageButton mBtnRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        mEditTextEmail = (EditText) findViewById(R.id.login_username);
+        mEditTextPassword = (EditText) findViewById(R.id.login_password);
+
+        mBtnLogin = (ImageButton) findViewById(R.id.masuk_btn);
+        mBtnRegister = (ImageButton) findViewById(R.id.daftar_btn);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            navigateToMainPage();
+        }
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEditTextEmail.getText().toString();
+                String password = mEditTextPassword.getText().toString();
+                handleLogin(email, password);
+
+
+            }
+        });
+
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToRegister();
+            }
+        });
+
+
     }
+
+    private void navigateToMainPage() {
+        Intent intent = new Intent(LoginPage.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void navigateToRegister() {
+        Intent intent = new Intent(LoginPage.this, RegisterPage.class);
+        startActivity(intent);
+    }
+
+    private void handleLogin(String email, String password) {
+
+        String emailCheck = mEditTextEmail.getText().toString();
+        String passwordCheck = mEditTextPassword.getText().toString();
+
+        if (emailCheck.equals("")) {
+            mEditTextEmail.setError("Email can't be blank");
+        } else if (passwordCheck.equals("")) {
+            mEditTextPassword.setError("Password can't be blank");
+        }  else {
+
+            mAuth.signInWithEmailAndPassword(emailCheck,passwordCheck).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Toast.makeText(LoginPage.this, "Berhasil Masuk", Toast.LENGTH_SHORT).show();
+                        navigateToMainPage();
+                    } else {
+                        Toast.makeText(LoginPage.this, "Gagal Masuk", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+
+    }
+
+
 }
--- app/src/main/java/pypoh/project/com/palapa/RegisterPage.java
+++ app/src/main/java/pypoh/project/com/palapa/RegisterPage.java
@@ -1,13 +1,111 @@
 package pypoh.project.com.palapa;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterPage extends AppCompatActivity {
+
+    private EditText mEditTextUsername;
+    private EditText mEditTextEmail;
+    private EditText mEditTextPassword;
+    private EditText mEditTextConfirmPassword;
+    private FirebaseAuth mAuth;
+    private ImageButton mBtnSignUp;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        mEditTextEmail = (EditText) findViewById(R.id.daftar_email);
+        mEditTextUsername = (EditText) findViewById(R.id.daftar_username);
+        mEditTextPassword = (EditText) findViewById(R.id.daftar_password);
+        mEditTextConfirmPassword = (EditText) findViewById(R.id.daftar_confirmpassword);
+        mBtnSignUp = (ImageButton) findViewById(R.id.daftar_btn);
+
+        mBtnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleRegister();
+            }
+        });
+
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+    private void handleRegister() {
+        String user = mEditTextUsername.getText().toString();
+        String email = mEditTextEmail.getText().toString();
+        String password = mEditTextPassword.getText().toString();
+        String con_pass = mEditTextConfirmPassword.getText().toString();
+
+
+        if (email.equals("")) {
+            mEditTextEmail.setError("Email can't be blank");
+        }  else if (password.equals("")) {
+            mEditTextPassword.setError("Password can't be blank");
+        } else if (password.length() < 6) {
+            mEditTextPassword.setError("At least 6 characters");
+        } else if (!con_pass.equals(password)) {
+            mEditTextConfirmPassword.setError("Password not match");
+        } else {
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        String userID = mEditTextUsername.getText().toString();
+                        DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+
+                        DatabaseReference ref1 = mRootRef.child("users").child(userID);
+
+                        ref1.child("Email").setValue(user.getEmail());
+                        ref1.child("Username").setValue(userID);
+                        Toast.makeText(RegisterPage.this, "Berhasil Mendaftar", Toast.LENGTH_SHORT).show();
+
+                        navigateToLogin();
+                    } else {
+                        Toast.makeText(RegisterPage.this, "Gagal Mendaftar", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+
+        }
+    }
+
+    private void navigateToLogin() {
+        Intent intent = new Intent(RegisterPage.this, LoginPage.class);
+        startActivity(intent);
+    }
+
+
 }
